55ba8664901b55de7edf2759e3e649bdf7084ec2
Mudassar Bhatti <mbhatti000@citymail.cuny.edu>
2019-09-27 17:34:09 -0400

    menubar and animation work

--- src/App.js
+++ src/App.js
@@ -9,31 +9,44 @@ class App extends Component {
        recipeList: [
           {
            image_url: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-           title: 'burrito'
+           title: 'cheese',
+           ingredients: ['1/2 cup mexican', 'taco-tuxedo', 'nacho libre', 'etc etc']
           },
           {
             image_url: 'https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-           title: 'burrito'
+            title: 'ham',
+            ingredients: ['1/2 cup mexican', 'taco-tuxedo', 'nacho libre', 'etc etc']
+
           },
           {
             image_url: 'https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-           title: 'burrito'
+           title: 'burrito',
+          ingredients: ['1/2 cup mexican', 'taco-tuxedo', 'nacho libre', 'etc etc'],
+
           },
           {
             image_url: 'https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-           title: 'burrito'
+           title: 'ham',
+            ingredients: ['1/2 cup mexican', 'taco-tuxedo', 'nacho libre', 'etc etc']
+
           },
           {
             image_url: 'https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-           title: 'burrito'
+           title: 'burrito',
+          ingredients: ['1/2 cup mexican', 'taco-tuxedo', 'nacho libre', 'etc etc']
+
           },
           {
             image_url: 'https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-           title: 'burrito'
+           title: 'burrito',
+          ingredients: ['1/2 cup mexican', 'taco-tuxedo', 'nacho libre', 'etc etc']
+
           },
           {
             image_url: 'https://images.pexels.com/photos/704569/pexels-photo-704569.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-           title: 'burrito'
+           title: 'burrito',
+          ingredients: ['whateva flava', 'anotha one', 'nachooooo', 'etc etc']
+
           }
        ],
        search : ''
@@ -50,10 +63,15 @@ class App extends Component {
     })
   }
 
+  handleClick = () => {
+    const wrapper = document.getElementById('wrapper')
+    wrapper.classList.toggle('is-nav-open')
+  }
+
   searchFoods = (event) => {
     event.preventDefault()
-
-    if (event.target.value) {
+    // console.log(event)
+    if (this.state.search) {
       const key = '115a287a7e2cfbd715b6be309c1c5075'
       fetch(`https://www.food2fork.com/api/search?key=${key}&q=${this.state.search}`)
         .then(response => response.json())
@@ -66,17 +84,46 @@ class App extends Component {
       }
     }
 
+    openNav = () => {
+      console.log("button clicked")
+      document.getElementById("mySidenav").style.width = "210px";
+    }
+    closeNav = () => {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+
+    displayModal = (e) => {
+      // let x  = document.getElementsByClassName('modal-content')[0]
+      // console.log(x)
+      // x.style.display = "none";
+      e.target.parentElement.children[1].style.display = "block"
+    }
+
+    closeModal = (e) => {
+      e.target.parentElement.style.display = "none";
+    }
     
     render() {
- 
-        const recipeList = this.state.recipeList.map((recipe, i) => (
-          <div key={i} className='recipe'>
-            <img src={recipe.image_url} alt='thumbnail' />
-            <p className='title'>{recipe.title}</p>
+      // const filteredRecipes = this.state.recipeList.filter(recipe => {
+      //   return recipe.title.toLowerCase().includes(this.state.search.toLowerCase())
+      // })
+      
+    const recipeList = this.state.recipeList.map((recipe, i) => (
+        <div key={i} className='recipe'><img src={recipe.image_url} alt='thumbnail' />
+          <p className='title'>{recipe.title}</p>
+
+            {/* <!-- The Modal --> */}
+          <div className="myModal">
+            <button className = "myBtn" onClick={this.displayModal}>Open Modal</button>
+            <div className="modal-content">
+              <span className="close" onClick={this.closeModal}>&times;</span>
+              <p>{recipe.title}</p>
+            </div>
           </div>
-        ))
-      
-      
+
+        </div>
+      ))
+
     return (
       <div>
         <div className="App">
@@ -89,11 +136,22 @@ class App extends Component {
               <input id='search' onChange={this.search} value={this.state.search} type='text' placeholder='Search Recipe'/>
             </form>
           </div>
-           
+            <div id="mySidenav" className="sidenav">
+              <div class="avatar"></div>
+                <a href="#" className="closebtn" onClick={this.closeNav}>&times;</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Clients</a>
+                <a href="#">Contact</a>
+            </div>
+            <span id="name">User</span>
+            <span style={{fontSize:"30px", cursor:"pointer", position:'relative'}} onClick={this.openNav}>&#9776;</span>
           <div className='secondSection'>
-            {recipeList}
+              {recipeList}
           </div>
-          <div className='thirdSection'></div>
+          <div className='thirdSection'>
+            {/* {filteredRecipes} */}
+          </div>
         </div>
       </div>
     )
