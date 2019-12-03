7f1b76fce73dc7b170de132edaa072b0d307750a
Mudassar Bhatti <mbhatti000@citymail.cuny.edu>
2019-12-02 19:55:28 -0500

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './Footer.css'
 
 export default function Footer() {
   return (
-    <div>
-      
+    <div className='footer'>
+      <li>Contact</li>
+      <li>Placeholder</li>
+      <li>Placeholder</li>
     </div>
   )
 }
